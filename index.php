f3edb16e4f6f81928b2401f5f9c22dccc53d729c
Prayant-M <prayant.m@gmail.com>
2023-06-12 23:58:05 +0530

    index(header and video add)

--- index.php
+++ index.php
@@ -1,1 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- This page - Landing Page -->
+
 <?php require 'header.php'; ?> 
+
+
+
+    <!-- Header Start -->
+    <div class="jumbotron jumbotron-fluid position-relative overlay-bottom" style="margin-bottom: 90px;">
+        <div class="container text-center my-5 py-5">
+            <h1 class="text-white mt-4 mb-4">PM AKADEMI</h1>
+            <h1 class="text-white display-1 mb-5">Introductory Course</h1>
+            <p  class="text-white">👇 Free 🎁 Introductory seminar 👇 </p>
+            <div class="mx-auto mb-5" style="width: 100%; max-width: 600px;">
+                <div class="input-group">
+                
+                
+     <!-- Introduction video  -->
+                <video
+    id="my-video"
+    class="video-js"
+    controls
+    preload="auto"
+    width="640"
+    height="264"
+    poster="seminar.jpg"
+    data-setup="{}"
+    oncontextmenu="return false;"
+  >
+    <source src="video/blank.mp4" type="video/mp4" />
+    <source src="MY_VIDEO.webm" type="video/webm" />
+    <p class="vjs-no-js">
+      To view this video please enable JavaScript, and consider upgrading to a
+      web browser that
+      <a href="https://videojs.com/html5-video-support/" target="_blank"
+        >supports HTML5 video</a
+      >
+    </p>
+  </video>
+    <!-- Introduction video  end  -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+</html>
