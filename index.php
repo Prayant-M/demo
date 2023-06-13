ccd40d143eb68c267b646f044a37530e8afa27a1
Prayant-M <prayant.m@gmail.com>
2023-06-13 19:58:05 +0530

    Carousel

--- index.php
+++ index.php
@@ -225,6 +225,12 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit.
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Carousel starts -->
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
     <!-- Template Javascript -->
     <script src="js/main.js"></script>
 </body>
